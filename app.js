1b2fd2b9751f63e5f8a8642046f63a2dcd021827
Paul Wynn <wfsl@proton.me>
2026-01-03 00:50:30 +0000

    Foundation: governance-first public landing scaffold

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById("openAce").addEventListener("click", () => {
+  alert(
+    "ACE will be activated here.\n\n" +
+    "This explainer is intentionally bounded and governance-first."
+  );
+});
